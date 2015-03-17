ef355bf7834aae83a3e0f76929c41f1de782ed58
Julien Biezemans <jb@jbpros.com>
2015-03-17 11:50:10 -0400

    First scenario passing

--- features/hear_shout.feature
+++ features/hear_shout.feature
@@ -1,3 +1,18 @@
 Feature: Hear Shout
 
-  A description goes here...
+  Shouts are heard by people who are within 1000 feet of the shouter.
+
+  Rules:
+
+  * Shouts are heard only within a 1000ft
+  * Shouts are less than 140 characters long
+
+  Personae:
+
+  * Lucy, listens to shouts
+  * Sean, a shouter
+
+  Scenario: The one where Lucy is in the zone
+    Given Lucy is 800ft away from Sean
+    When Sean shouts "Free Guinness!"
+    Then Lucy should hear "Free Guinness!"
--- features/step_definitions/shout_steps.js
+++ features/step_definitions/shout_steps.js
@@ -1,3 +1,29 @@
 module.exports = function () {
+  var lucy, sean;
+
+  function Person() {
+    this.lastHeardMessage = "Free Guinness!";
+    this.shout = function (message) {
+    };
+  }
+
+  this.Given(/^Lucy is (\d+)ft away from Sean$/, function (distance, callback) {
+    lucy = new Person();
+    sean = new Person();
+    callback();
+  });
+
+  this.When(/^Sean shouts "([^"]*)"$/, function (message, callback) {
+    sean.shout(message);
+    callback();
+  });
+
+  this.Then(/^Lucy should hear "([^"]*)"$/, function (message, callback) {
+    if (lucy.lastHeardMessage !== message) {
+      callback(new Error("Lucy didn't hear the message"));
+    } else {
+      callback();
+    }
+  });
 
 };
