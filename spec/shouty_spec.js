c2979099dd447b1b79bb1018b9b447e4c44796db
Julien Biezemans <jb@jbpros.com>
2015-03-25 10:36:07 +0100

    Add dependencies

--- spec/shouty_spec.js
+++ spec/shouty_spec.js
@@ -1,3 +1,9 @@
+var chai = require('chai');
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var expect = chai.expect;
+chai.use(sinonChai);
+
 describe("Shouty", function () {
 
   it("does things...", function () {
