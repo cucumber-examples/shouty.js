9b305732f98a1b3c40ac09e4cdf0cc27c3b4f944
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-05-06 00:22:29 +0200

    Use a World that just uses our domain facade. Weird callbacky /cc @jbpros

--- features/step_definitions/shout_steps.js
+++ features/step_definitions/shout_steps.js
@@ -7,33 +7,34 @@ module.exports = function () {
     "Balliol College"   : [51.7550014, -1.2580754],
     "Trafalgar Square"  : [51.508039,  -0.128069]
   };
-  var shouty;
   var theShout;
 
-  this.Before(function (callback) {
-    shouty = new Shouty();
+  this.World = function (callback) {
+    // @jbpros - requiring a callback here is a bit weird.
+    // I'd like to just say: this.World = Shouty;
     callback();
-  });
+    return new Shouty();
+  };
 
   this.Given(/^"([^"]*)" is at "([^"]*)"$/, function (personName, locationName, callback) {
     var location = locations[locationName];
-    shouty.personIsAt(personName, location);
+    this.personIsAt(personName, location);
     callback();
   });
 
   this.When(/^"([^"]*)" shouts$/, function (personName, callback) {
     theShout = "Anyone there?";
-    shouty.personShouts(personName, theShout);
+    this.personShouts(personName, theShout);
     callback();
   });
 
   this.Then(/^"([^"]*)" hears the shout$/, function (personName, callback) {
-    assert.deepEqual(shouty.messagesHeardBy(personName), [theShout]);
+    assert.deepEqual(this.messagesHeardBy(personName), [theShout]);
     callback();
   });
 
   this.Then(/^"([^"]*)" doesn't hear anything$/, function (personName, callback) {
-    assert.deepEqual(shouty.messagesHeardBy(personName), []);
+    assert.deepEqual(this.messagesHeardBy(personName), []);
     callback();
   });
 
