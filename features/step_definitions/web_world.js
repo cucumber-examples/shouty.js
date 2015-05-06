dcf126902bce773fe14cd17a9de9d70f1ead5af4
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-05-06 14:05:01 +0200

    Make selenium work, kinda

--- features/step_definitions/web_world.js
+++ features/step_definitions/web_world.js
@@ -1,21 +1,10 @@
-var Browser = require('zombie');
-Browser.localhost('example.com', 3000);
-var browser = new Browser();
+var webdriver = require('selenium-webdriver');
 
 module.exports.WebWorld = function (callback) {
   this.personIsAt = function(personName, geoLocation, callback) {
-    browser.visit('/people/' +
-                  personName +
-                  '?lat=' + geoLocation[0] +
-                  '&lon=' + geoLocation[1],
-                  callback);
-  };
-
-  this.personShouts = function (personName, message, callback) {
-  };
-
-  this.messagesHeardBy = function (personName, callback) {
-    return [];
+    var browser = new webdriver.Builder()
+      .forBrowser('firefox')
+      .build();
   };
 
   callback();
