31ba2ca5522885d94b233c5d7cea3f4f276cb2ef
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-05-06 15:12:36 +0200

    We're shouting with Selenium

--- features/step_definitions/selenium_world.js
+++ features/step_definitions/selenium_world.js
@@ -1,17 +1,31 @@
 var webdriver = require('selenium-webdriver');
+var By = webdriver.By;
 
 module.exports.SeleniumWorld = function (callback) {
+  var browsers = {};
+
   this.personIsAt = function(personName, geoLocation, callback) {
     var browser = new webdriver.Builder()
       .forBrowser('firefox')
       .build();
 
+    browsers[personName] = browser;
+
     browser.get(
       'http://localhost:3000/people/' +
       personName +
       '?lat=' + geoLocation[0] +
-      '&lot=' + geoLocation[1]
+      '&lon=' + geoLocation[1]
     ).then(callback);
+  };
+
+  this.personShouts = function (personName, message, callback) {
+    var browser = browsers[personName];
+    browser.findElement(By.name('message')).sendKeys(message);
+    browser.findElement(By.name('shout')).click().then(function () {
+      callback();
+    });
+
   };
 
   callback();
