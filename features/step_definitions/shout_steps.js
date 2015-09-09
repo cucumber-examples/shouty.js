f879c34dedf1ca82f5fde25e1f5327c47e31f44d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-09-09 12:19:28 +0100

    First step green

--- features/step_definitions/shout_steps.js
+++ features/step_definitions/shout_steps.js
@@ -1,6 +1,10 @@
 var assert = require('assert');
 var Shouty = require('../../lib/shouty');
 
+var bridgeHouse = [53.4720597,-2.2998615];
+var theQuays    = [53.4709477,-2.2938976];
+var piccadilly  = [53.4774029,-2.2309325];
+
 module.exports = function () {
   this.World = function(callback) {
     callback();
@@ -9,7 +13,9 @@ module.exports = function () {
 
   this.Given(/^Joanne is (\d+) km away from Fred$/, function (arg1, callback) {
     // Write code here that turns the phrase above into concrete actions
-    callback.pending();
+    this.personIsIn("Joanne", bridgeHouse);
+    this.personIsIn("Fred", piccadilly);
+    callback();
   });
 
   this.When(/^Fred shouts$/, function (callback) {
