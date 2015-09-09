cc9f45bd27f33d52a70fcea352e026b8c07233ff
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-09-09 11:50:22 +0100

    Added some pending step definitions. Make pretty default format.

--- features/step_definitions/shout_steps.js
+++ features/step_definitions/shout_steps.js
@@ -6,4 +6,20 @@ module.exports = function () {
     callback();
     return new Shouty();
   };
+
+  this.Given(/^Joanne is (\d+) km away from Fred$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.When(/^Fred shouts$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Then(/^Joanne does not hear Fred's message$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
 };
