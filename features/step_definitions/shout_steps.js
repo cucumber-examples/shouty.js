302b44eb770708a031ce70e6562b50bc95d94291
Julien Biezemans <jb@jbpros.com>
2015-09-30 17:05:45 +0200

    First feature passing

--- features/step_definitions/shout_steps.js
+++ features/step_definitions/shout_steps.js
@@ -6,15 +6,24 @@ function Shouter(network, position) {
   this.shout = function (shout) {
     network.broadcast(this, shout);
   };
+
+  this.hear = function (shout) {
+    this.lastHeardShout = shout;
+  };
 }
 
 function Network() {
   var shouters = [];
 
   this.broadcast = function (shouter, shout) {
+    var tooShort = shout.length < 1;
+    var tooLong = shout.length > 143;
+    if (tooShort || tooLong) return;
+
     shouters.forEach(function (listener) {
-      if (Math.abs(listener.position - shouter.position) <= 500) {
-        listener.lastHeardMessage = shout;
+      var distance = Math.abs(listener.position - shouter.position);
+      if (distance <= 500) {
+        listener.hear(shout);
       }
     });
   };
@@ -25,34 +34,30 @@ function Network() {
 }
 
 module.exports = function () {
-  var sean, lucy;
+  var sean, lucy, shouters = {};
 
   this.Given(/^Lucy is (\d+) meters away from Sean$/, function (distance) {
     var lucyPosition = distance;
     var network = new Network();
     sean = new Shouter(network, 0);
     lucy = new Shouter(network, lucyPosition);
+    shouters.Sean = sean;
+    shouters.Lucy = lucy;
   });
 
   this.When(/^Sean shouts "([^"]*)"$/, function (shout) {
     sean.shout(shout);
   });
 
-  this.Then(/^Lucy should hear "([^"]*)"$/, function (shout) {
-    if (lucy.lastHeardMessage != shout) {
-      throw new Error("Expected Lucy to hear " + shout + ", but she heard " + lucy.lastHeardMessage);
+  this.Then(/^(\w+) should hear "([^"]*)"$/, function (name, shout) {
+    if (shouters[name].lastHeardShout != shout) {
+      throw new Error("Expected " + name + " to hear " + shout + ", but they heard " + shouters[name].lastHeardShout);
     }
   });
 
-  this.Then(/^Sean should hear "([^"]*)"$/, function (shout) {
-    if (sean.lastHeardMessage != shout) {
-      throw new Error("Expected Sean to hear " + shout + ", but he heard " + sean.lastHeardMessage);
-    }
-  });
-
-  this.Then(/^Lucy should not hear "([^"]*)"$/, function (shout) {
-    if (lucy.lastHeardMessage == shout) {
-      throw new Error("Expected Lucy not to hear " + shout + ", but she heard it.");
+  this.Then(/^(\w+) should not hear "([^"]*)"$/, function (name, shout) {
+    if (shouters[name].lastHeardShout == shout) {
+      throw new Error("Expected " + name + " not to hear " + shout + ", but they heard it.");
     }
   });
 };
