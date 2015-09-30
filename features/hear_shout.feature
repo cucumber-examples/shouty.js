db9a2d7ea2299f09a5c1c0df755fb149baaa6ff6
Julien Biezemans <jb@jbpros.com>
2015-09-30 13:16:56 +0200

    First scenario passing

--- features/hear_shout.feature
+++ features/hear_shout.feature
@@ -1,3 +1,28 @@
 Feature: Hear Shout
+  People want to shout to other people around them.
 
-  A description goes here...
+  Rules:
+    - shouts are heard <= 500 meters
+    - shout length must be 1 to 143 characters only
+
+    - don't bother when the network is down
+    - requires known location
+    - shouts are heard immediately, cannot be heard later, cannot be unheard
+    - you always get the whole shout, even if you move when it's shouted
+    - you hear yourself
+
+  Personae:
+    - Sean: a shouter
+    - Lucy: a listener
+
+  Scenario: Lucy can hear Sean
+    Given Lucy is 500 meters away from Sean
+    When Sean shouts "Hello World!"
+    Then Lucy should hear "Hello World!"
+    And Sean should hear "Hello World!"
+
+  Scenario: Lucy cannot hear Sean
+    Given Lucy is 501 meters away from Sean
+    When Sean shouts "Hello World!"
+    Then Lucy should not hear "Hello World!"
+    But Sean should hear "Hello World!"
--- features/step_definitions/shout_steps.js
+++ features/step_definitions/shout_steps.js
@@ -1,9 +1,35 @@
-var assert = require('assert');
-var Shouty = require('../../lib/shouty');
+function Shouter() {
+  this.shout = function () {};
+
+  this.lastHeardMessage = "Hello World!";
+}
 
 module.exports = function () {
-  this.World = function(callback) {
-    callback();
-    return new Shouty();
-  };
+  var sean, lucy;
+
+  this.Given(/^Lucy is (\d+) meters away from Sean$/, function (distance) {
+    sean = new Shouter();
+    lucy = new Shouter();
+  });
+
+  this.When(/^Sean shouts "([^"]*)"$/, function (shout) {
+    sean.shout(shout);
+  });
+
+  this.Then(/^Lucy should hear "([^"]*)"$/, function (shout) {
+    if (lucy.lastHeardMessage != shout) {
+      throw new Error("Expected Lucy to hear " + shout + ", but she heard " + lucy.lastHeardMessage);
+    }
+  });
+
+  this.Then(/^Sean should hear "([^"]*)"$/, function (shout) {
+    if (sean.lastHeardMessage != shout) {
+      throw new Error("Expected Sean to hear " + shout + ", but he heard " + sean.lastHeardMessage);
+    }
+  });
+
+  this.Then(/^Lucy should not hear "([^"]*)"$/, function (arg1, callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
 };
