4be6c55ca1746b618f73dd8313a663a7fbe6eca3
Julien Biezemans <jb@jbpros.com>
2016-03-03 14:26:59 +0100

    Bump Cucumber

--- features/step_definitions/shout_steps.js
+++ features/step_definitions/shout_steps.js
@@ -2,8 +2,6 @@ var assert = require('assert');
 var Shouty = require('../../lib/shouty');
 
 module.exports = function () {
-  this.World = function(callback) {
-    callback();
-    return new Shouty();
+  this.World = function() {
   };
 };
