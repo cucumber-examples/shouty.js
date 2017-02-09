b1e227f389c69fc0b4a5e15e4524c68146562b90
Seb Rose <seb@claysnow.co.uk>
2017-02-09 13:33:27 +0000

    Get JS startpoint to same state as Java/CSharp, so workbooks align

--- features/support/world.js
+++ features/support/world.js
@@ -1,8 +1,6 @@
 var {defineSupportCode} = require('cucumber');
-var Shouty = require('../../lib/shouty');
 
 function CustomWorld() {
-  this.shouty = new Shouty()
 }
 
 defineSupportCode(function({setWorldConstructor}) {
