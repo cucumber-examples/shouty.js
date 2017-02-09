62ead70d7ac915382c71356e35ab0ba70d71d6fa
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-02-09 21:42:05 +0000

    Remove semicolon

--- test/coordinate.test.js
+++ test/coordinate.test.js
@@ -4,13 +4,13 @@ const Coordinate = require('../lib/coordinate')
 describe(Coordinate.name, () => {
 
   it("should calculate the distance from itself", () => {
-    var a = new Coordinate(0, 0);
+    var a = new Coordinate(0, 0)
     assert.equal(a.distanceFrom(a), 0)
   })
 
   it("should calculate the distance from another coordinate along X axis", ()=>{
-    var a = new Coordinate(0, 0);
-    var b = new Coordinate(1000, 0);
+    var a = new Coordinate(0, 0)
+    var b = new Coordinate(1000, 0)
 
     assert.equal(a.distanceFrom(b), 1000)
 
