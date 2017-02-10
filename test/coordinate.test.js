9384c81f1b8ae775e81968a9a786142ee77457d1
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2017-02-10 13:56:32 +0000

    Use const

--- test/coordinate.test.js
+++ test/coordinate.test.js
@@ -4,15 +4,14 @@ const Coordinate = require('../lib/coordinate')
 describe(Coordinate.name, () => {
 
   it("should calculate the distance from itself", () => {
-    var a = new Coordinate(0, 0)
+    const a = new Coordinate(0, 0)
     assert.equal(a.distanceFrom(a), 0)
   })
 
   it("should calculate the distance from another coordinate along X axis", ()=>{
-    var a = new Coordinate(0, 0)
-    var b = new Coordinate(1000, 0)
+    const a = new Coordinate(0, 0)
+    const b = new Coordinate(1000, 0)
 
     assert.equal(a.distanceFrom(b), 1000)
-
   })
 })
