6c307c61e719c2f3bd00511da199c8f7a7c5aacc
Steve Tooke <tooky@cucumber.io>
2017-03-24 16:09:49 +0000

    Hoist functions for readability

--- features/step_definitions/shout_steps.js
+++ features/step_definitions/shout_steps.js
@@ -2,48 +2,9 @@ const assert = require('assert')
 const {defineSupportCode} = require('cucumber')
 const {Task, Question} = require('../../lib/screenplay')
 const Shouty = require('../../lib/shouty')
-
 const {TalkToShoutyAPI, TalkToRestAPI} = require('../support/abilities')
 
-function getCoreAbility() {
-  if (process.env.SHOUTY_ADAPTER === 'rest')
-    return TalkToRestAPI
-  else
-    return TalkToShoutyAPI
-}
-
-const getCoreAbilityAs = function (actor) {
-  return getCoreAbility().as(actor)
-}
-
-function startAtLocation(location) {
-  return async (actor) => getCoreAbilityAs(actor).identifyAs({
-    as: actor.name,
-    at: location
-  })
-}
-
-function shoutMessage(message) {
-  return async actor => getCoreAbilityAs(actor).shout(message)
-}
-
-function checkThat(question, match) {
-  return async actor => {
-    const answer = await actor.answers(question)
-    return match(answer)
-  }
-}
-
-function lastHeardMessageFrom(shouterName) {
-  return async actor => {
-    const heardMessages = await getCoreAbilityAs(actor).getMessagesHeard()
-    // TODO: filter messages, make sure it was sent by this._shouterName
-    return heardMessages[heardMessages.length - 1]
-  }
-}
-
 const DEFAULT_MESSAGE = 'Hi folks!!!'
-
 const hasContents = expected => actual => assert.equal(actual, expected)
 
 defineSupportCode(({Before, Given, When, Then}) => {
@@ -74,5 +35,41 @@ defineSupportCode(({Before, Given, When, Then}) => {
   Then('{actor} should hear nothing', async function (listener) {
     return 'pending'
   })
-
 })
+
+function startAtLocation(location) {
+  return async (actor) => getCoreAbilityAs(actor).identifyAs({
+    as: actor.name,
+    at: location
+  })
+}
+
+function shoutMessage(message) {
+  return async actor => getCoreAbilityAs(actor).shout(message)
+}
+
+function checkThat(question, match) {
+  return async actor => {
+    const answer = await actor.answers(question)
+    return match(answer)
+  }
+}
+
+function lastHeardMessageFrom(shouterName) {
+  return async actor => {
+    const heardMessages = await getCoreAbilityAs(actor).getMessagesHeard()
+    // TODO: filter messages, make sure it was sent by this._shouterName
+    return heardMessages[heardMessages.length - 1]
+  }
+}
+
+function getCoreAbility() {
+  if (process.env.SHOUTY_ADAPTER === 'rest')
+    return TalkToRestAPI
+  else
+    return TalkToShoutyAPI
+}
+
+function getCoreAbilityAs(actor) {
+  return getCoreAbility().as(actor)
+}
