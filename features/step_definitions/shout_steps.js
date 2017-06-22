9902d73d60768e2aa17527f26295dfeed1883d08
Charles Rudolph <charles.w.rudolph@gmail.com>
2017-06-22 16:26:28 +0100

    update dependency: cucumber

--- features/step_definitions/shout_steps.js
+++ features/step_definitions/shout_steps.js
@@ -2,33 +2,30 @@ const assert = require('assert')
 const Shouty = require('../../lib/shouty')
 const Coordinate = require('../../lib/coordinate')
 
-const {defineSupportCode} = require('cucumber')
+const {Before, Given, When, Then} = require('cucumber')
 
-defineSupportCode(({Before, Given, When, Then}) => {
-  const ARBITARY_MESSAGE = 'Hello, world'
-  let shouty
-  Before(function() {
-    shouty = new Shouty()
-  })
+const ARBITARY_MESSAGE = 'Hello, world'
+let shouty
+Before(function() {
+  shouty = new Shouty()
+})
 
-  Given('Lucy is at {int}, {int}', function (x, y) {
-    shouty.setLocation('Lucy', new Coordinate(x, y))
-  })
+Given('Lucy is at {int}, {int}', function (x, y) {
+  shouty.setLocation('Lucy', new Coordinate(x, y))
+})
 
-  Given('Sean is at {int}, {int}', function (x, y) {
-    shouty.setLocation('Sean', new Coordinate(x, y))
-  })
+Given('Sean is at {int}, {int}', function (x, y) {
+  shouty.setLocation('Sean', new Coordinate(x, y))
+})
 
-  When('Sean shouts', function () {
-    shouty.shout('Sean', ARBITARY_MESSAGE)
-  })
+When('Sean shouts', function () {
+  shouty.shout('Sean', ARBITARY_MESSAGE)
+})
 
-  Then('Lucy should hear Sean', function () {
-    assert.equal(shouty.getMessagesHeardBy('Lucy').size, 1)
-  })
+Then('Lucy should hear Sean', function () {
+  assert.equal(shouty.getMessagesHeardBy('Lucy').size, 1)
+})
 
-  Then('Lucy should hear nothing', function () {
-    assert.equal(shouty.getMessagesHeardBy('Lucy').size, 0)
-  })
-
+Then('Lucy should hear nothing', function () {
+  assert.equal(shouty.getMessagesHeardBy('Lucy').size, 0)
 })
