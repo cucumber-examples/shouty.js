4e138305b8ab6792d0dd21b8cff862fe827f82c4
Seb Rose <seb@claysnow.co.uk>
2017-11-12 10:12:00 -0500

    Check that this works with current cucumber-js before merging

--- features/step_definitions/shout_steps.js
+++ features/step_definitions/shout_steps.js
@@ -1,31 +1,33 @@
-const assert = require('assert')
-const { Before, Given, When, Then } = require('cucumber')
-const Shouty = require('../../lib/shouty')
-const Coordinate = require('../../lib/coordinate')
+'use strict'
 
-const ARBITARY_MESSAGE = 'Hello, world'
+const assert = require('assert');
+const { Before, Given, When, Then } = require('cucumber');
+const Shouty = require('../../lib/shouty');
+const Coordinate = require('../../lib/coordinate');
+
+const ARBITARY_MESSAGE = 'Hello, world';
 let shouty
 
 Before(function() {
-  shouty = new Shouty()
-})
+  shouty = new Shouty();
+});
 
 Given('Lucy is at {int}, {int}', function (x, y) {
-  shouty.setLocation('Lucy', new Coordinate(x, y))
-})
+  shouty.setLocation('Lucy', new Coordinate(x, y));
+});
 
 Given('Sean is at {int}, {int}', function (x, y) {
-  shouty.setLocation('Sean', new Coordinate(x, y))
-})
+  shouty.setLocation('Sean', new Coordinate(x, y));
+});
 
 When('Sean shouts', function () {
-  shouty.shout('Sean', ARBITARY_MESSAGE)
-})
+  shouty.shout('Sean', ARBITARY_MESSAGE);
+});
 
 Then('Lucy should hear Sean', function () {
-  assert.equal(shouty.getMessagesHeardBy('Lucy').size, 1)
-})
+  assert.equal(shouty.getShoutsHeardBy('Lucy').size, 1);
+});
 
 Then('Lucy should hear nothing', function () {
-  assert.equal(shouty.getMessagesHeardBy('Lucy').size, 0)
-})
+  assert.equal(shouty.getShoutsHeardBy('Lucy').size, 0);
+});
--- test/coordinate.test.js
+++ test/coordinate.test.js
@@ -1,17 +1,26 @@
-const assert = require('assert')
-const Coordinate = require('../lib/coordinate')
+'use strict'
+
+const assert = require('assert');
+const Coordinate = require('../lib/coordinate');
 
 describe('Coordinate', () => {
 
   it("should calculate the distance from itself", () => {
-    const a = new Coordinate(0, 0)
-    assert.equal(a.distanceFrom(a), 0)
-  })
+    const a = new Coordinate(0, 0);
+    assert.equal(a.distanceFrom(a), 0);
+  });
 
   it("should calculate the distance from another coordinate along X axis", ()=>{
-    const a = new Coordinate(0, 0)
-    const b = new Coordinate(1000, 0)
+    const a = new Coordinate(0, 0);
+    const b = new Coordinate(600, 0);
 
-    assert.equal(a.distanceFrom(b), 1000)
-  })
-})
+    assert.equal(a.distanceFrom(b), 600)
+  });
+
+  // it("should calculate the distance from another coordinate", ()=>{
+  //   const a = new Coordinate(0, 0);
+  //   const b = new Coordinate(300, 400);
+  //
+  //   assert.equal(a.distanceFrom(b), 500)
+  // });
+});
