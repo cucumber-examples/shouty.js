d5a86234c44a89db80087a5b2816f6eef99f7ac0
Seb Rose <seb@claysnow.co.uk>
2021-01-14 16:42:12 +0000

    Replace deprecated assert.equals

--- test/coordinate.test.js
+++ test/coordinate.test.js
@@ -5,20 +5,20 @@ describe('Coordinate', () => {
 
   it("should calculate the distance from itself", () => {
     const a = new Coordinate(0, 0)
-    assert.equal(a.distanceFrom(a), 0)
+    assert.strictEqual(a.distanceFrom(a), 0)
   })
 
   it("should calculate the distance from another coordinate along X axis", ()=>{
     const a = new Coordinate(0, 0)
     const b = new Coordinate(600, 0)
 
-    assert.equal(a.distanceFrom(b), 600)
+    assert.strictEqual(a.distanceFrom(b), 600)
   })
 
   // it("should calculate the distance from another coordinate", ()=>{
   //   const a = new Coordinate(0, 0)
   //   const b = new Coordinate(300, 400)
   //
-  //   assert.equal(a.distanceFrom(b), 500)
+  //   assert.strictEqual(a.distanceFrom(b), 500)
   // })
 })
