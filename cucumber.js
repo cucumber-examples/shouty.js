5c78580025e4a899ff65c77a3d5a079a225f6fa8
Seb Rose <seb@claysnow.co.uk>
2021-01-14 16:40:44 +0000

    Update start point to include pretty-formatter

--- cucumber.js
+++ cucumber.js
@@ -1,3 +1,3 @@
 module.exports = {
-  default: `--format-options '{"snippetInterface": "synchronous"}'`
+  default: `--publish-quiet --format-options '{"snippetInterface": "synchronous","theme":{"feature keyword":["magenta","bold"],"rule keyword":["magenta","bold"],"scenario keyword":["magenta","bold"],"step keyword":["blue","bold"],"step text":["green"],"step status":["red"]}}'`
 }
