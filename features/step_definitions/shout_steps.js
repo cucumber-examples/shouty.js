10c669f85681899f1371514778e9186702357f8c
Seb Rose <seb@claysnow.co.uk>
2021-01-20 17:06:51 +0000

    Update to use strictEqual

--- features/step_definitions/shout_steps.js
+++ features/step_definitions/shout_steps.js
@@ -23,9 +23,9 @@ When('Sean shouts', function () {
 })
 
 Then('Lucy should hear Sean', function () {
-  assert.equal(shouty.getShoutsHeardBy('Lucy').size, 1)
+  assert.strictEqual(shouty.getShoutsHeardBy('Lucy').size, 1)
 })
 
 Then('Lucy should hear nothing', function () {
-  assert.equal(shouty.getShoutsHeardBy('Lucy').size, 0)
+  assert.strictEqual(shouty.getShoutsHeardBy('Lucy').size, 0)
 })
